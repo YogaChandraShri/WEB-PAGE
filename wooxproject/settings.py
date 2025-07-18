e4d05d148be83d83550dc507bdd7317e2978f3e0
Yogachandra Shri <yogachandrashri18@gmail.com>
2025-07-18 15:26:51 +0530

    settings commit

--- wooxproject/settings.py
+++ wooxproject/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-(k&#c++=kg+-hnqmaqo*ft31b*do_moojl4b@3&-ha)ptff0*$
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
